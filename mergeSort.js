fa329730017f0843a3ce8d62d55733f0c5f3f341
Jordan Heffernan <jheff2@gmail.com>
2019-03-07 15:08:58 -0800

    merge sort from sratch

--- mergeSort.js
+++ mergeSort.js
@@ -1,1 +1,60 @@
 'use strict';
+
+const data = [
+  89, 30, 25, 32, 72,
+  70, 51, 42, 25, 24,
+  53, 55, 78, 50, 148, 32, 26, 2,
+  14, 33, 45, 72, 521, 88, 27, 68,
+  15, 62, 93, 98, 716, 46, 87, 28,
+  65, 38, 67, 16, 823, 69, 64, 91,
+  9, 70, 81, 27, 97, 88, 3, 7, 46,
+  13, 11, 64, 76, 338, 28, 13, 17,
+  69, 90, 1, 6, 7, 9, 73, 80, 98,
+  46, 27, 22, 87, 46, 39, 42, 51,
+  54, 84, 34, 53, 714, 5];
+
+let count = 0;
+function mSort(arr) {
+  count++;
+  if (arr.length < 2) {
+    return arr;
+  }
+  let middle = Math.floor(arr.length / 2);
+  let leftSide = arr.slice(0, middle);
+  let rightSide = arr.slice(middle);
+  mSort(leftSide);
+  mSort(rightSide);
+  merge(leftSide, rightSide, arr);
+  return arr;
+}
+
+function merge(left, right, arr) {
+  let i = 0; //keep track of left
+  let j = 0; //keep track of right
+  let k = 0; //keep track of new array
+  while ((i < left.length) && (j < right.length)) {
+    if (left[i] < right[j]) {
+      arr[k] = left[i];
+      i++;
+    } else {
+      arr[k] = right[j];
+      j++;
+    }
+    k++;
+  }
+  if (i < left.length) {
+    for (i; i < left.length; i++) {
+      arr[k] = left[i];
+      k++;
+    }
+  }
+  if (j < right.length) {
+    for (j; j < right.length; j++) {
+      arr[k] = right[j];
+      k++;
+    }
+  }
+  return arr;
+}
+console.log(mSort(data));
+console.log(count);
--- quickSort.js
+++ quickSort.js
@@ -1,64 +1,40 @@
 'use strict';
 
 const data = [
-  89, 30, 25, 32, 72];
-// 70, 51, 42, 25, 24
-// 53, 55, 78, 50, 13, 40, 48, 32, 26, 2,
-// 14, 33, 45, 72, 56, 44, 21, 88, 27, 68,
-// 15, 62, 93, 98, 73, 28, 16, 46, 87, 28,
-// 65, 38, 67, 16, 85, 63, 23, 69, 64, 91,
-// 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46,
-// 13, 11, 64, 76, 31, 26, 38, 28, 13, 17,
-// 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98,
-// 46, 27, 22, 87, 49, 83, 6, 39, 42, 51,
-// 54, 84, 34, 53, 78, 40, 14, 5];
+  89, 30, 25, 32, 72,
+  70, 51, 42, 25, 24,
+  53, 55, 78, 50, 13, 40, 48, 32, 26, 2,
+  14, 33, 45, 72, 56, 44, 21, 88, 27, 68,
+  15, 62, 93, 98, 73, 28, 16, 46, 87, 28,
+  65, 38, 67, 16, 85, 63, 23, 69, 64, 91,
+  9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46,
+  13, 11, 64, 76, 31, 26, 38, 28, 13, 17,
+  69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98,
+  46, 27, 22, 87, 49, 83, 6, 39, 42, 51,
+  54, 84, 34, 53, 78, 40, 14, 5];
 
-
-// function qSort(arr, start = 0 , end = arr.length) {
-//   if (start >= end) {
-//     return arr;
-//   }
-//   const middle = partition(arr, start, end);
-//   arr = qSort(arr, start, middle);
-//   arr = qSort(arr, middle + 1, end);
-//   return arr;
-// }
-
-// function partition(arr, start, end) {
-//   let pivot = arr[end - 1];
-//   let j = start;
-//   let i = start - 1;
-//   while (j <= (end - 1)) {
-//     if (arr[j] < pivot) {
-//       i++;
-//       swap(arr, i, j);
-//     }
-//     j++;
-//   }
-//   swap(arr, end - 1, j);
-//   return j;
-// }
-
-function quickSort(array, start=0, end=array.length) {
+let count = 0;
+function qSort(arr, start = 0, end = arr.length) {
+  count++;
   if (start >= end) {
-    return array;
+    return arr;
   }
-  const middle = partition(array, start, end);
-  array = quickSort(array, start, middle);
-  array = quickSort(array, middle + 1, end);
-  return array;
+  const middle = partition(arr, start, end);
+  arr = qSort(arr, start, middle);
+  arr = qSort(arr, middle + 1, end);
+  return arr;
 }
 
-function partition(array, start, end) {
-  const pivot = array[end - 1];
+function partition(arr, start, end) {
+  let pivot = arr[end - 1];
   let j = start;
-  for (let i=start; i<end - 1; i++) {
-    if (array[i] <= pivot) {
-      swap(array, i, j);
+  for (let i = start; i < end - 1; i++) {
+    if (arr[i] <= pivot) {
+      swap(arr, i, j);
       j++;
     }
   }
-  swap(array, end-1, j);
+  swap(arr, end - 1, j);
   return j;
 }
 
@@ -68,4 +44,5 @@ function swap(array, i, j) {
   array[j] = temp;
 }
 
-console.log(quickSort(data));
+console.log(qSort(data));
+console.log(count);
